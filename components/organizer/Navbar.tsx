f0cc71f0563a8d57496006916025dbae35489050
syam-ts <syamnandhu3@gmail.com>
2025-07-27 17:00:50 +0530

    feat: add new event form

--- components/organizer/Navbar.tsx
+++ components/organizer/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
@@ -65,13 +66,21 @@ const Navbar = () => {
                 >
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white -800 md:-900 -700">
                         <li>
-                            <a
-                                href="#"
+                            <Link
+                                href="/organizer/home"
                                 className="block py-2 px-3 text-white bg-blue-700 font-bold rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:-500"
                                 aria-current="page"
                             >
                                 Home
-                            </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/organizer/new-event"
+                                className="block py-2 px-3 text-gray-900 rounded-sm font-bold hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:-blue-500 -gray-700 -white md:-transparent -700"
+                            >
+                                New Event sd
+                            </Link>
                         </li>
                         <li>
                             <a
@@ -79,14 +88,6 @@ const Navbar = () => {
                                 className="block py-2 px-3 text-gray-900 rounded-sm font-bold hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:-blue-500 -gray-700 -white md:-transparent -700"
                             >
                                 About
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-gray-900 rounded-sm font-bold hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0  md:-blue-500 -gray-700 -white md:-transparent -700"
-                            >
-                                Services
                             </a>
                         </li>
                         <li>
