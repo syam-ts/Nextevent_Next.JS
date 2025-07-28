661d2a724175b28f57f683ee3387c55ad407a256
syam-ts <syamnandhu3@gmail.com>
2025-07-28 19:58:36 +0530

    feat: add organizer profile

--- app/organizer/profile/page.tsx
+++ app/organizer/profile/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useSelector } from "react-redux";
 
 const page = () => {
-  return (
-    <div>Profile page</div>
-  )
-}
+  const organizer = useSelector(
+    (state: any) => state.organizer.currentOrganizer
+  );
 
-export default page
+  return (
+    <div className="bg-white h-screen">
+      <div className="container mx-auto px-8 py-44 bg-white">
+        <div className="border border-gray-300 rounded-2xl">
+          <div className="h-44 w-44">
+            <Image
+              src="/logo.png"
+              alt="logo"
+              height={100}
+              width={100}
+              className="w-full h-full object-center"
+            />
+          </div>
+          <div>
+            <div className="flex justify-between gap-3 px-20">
+              <div>
+                <div className="blue-gray h-6 text-black font-extrabold">
+                  {organizer.name}
+                </div>
+                <div className="font-normal text-gray-600">
+                  {organizer.email}
+                </div>
+              </div>
+
+              <div className="text-black grid">
+                <p>{organizer.mobile}</p>
+                <p>{organizer.organizationName}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
