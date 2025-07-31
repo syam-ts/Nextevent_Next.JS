1edfe4cdc3770af417e5432cacc1daf99372c772
syam-ts <syamnandhu3@gmail.com>
2025-07-31 11:35:05 +0530

    feat: add role selection component to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import Footer from "../components/common/Footer";
 import LandingPageBanner from "../components/common/LandingPageBanner";
+import LandingPageRoleSelection from "../components/common/LandingPageRoleSelection";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full bg-white">
-      <main className="flex justify-center">
+    <div className="h-screen w-full"
+    style={{
+            animation: "slideUp 0.6s ease-out",
+          }}
+    >
+      <main className=" justify-center "
+       >
 
 
 <LandingPageBanner />
 
+<LandingPageRoleSelection />
         
 
       </main>
