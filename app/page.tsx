d95ac67cede07e6056c2a3ee779c3bea6f4fa251
syam-ts <syamnandhu3@gmail.com>
2025-07-31 11:44:26 +0530

    feat: add and complete landinpage components

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 import Footer from "../components/common/Footer";
 import LandingPageBanner from "../components/common/LandingPageBanner";
+import LandingPageCommunity from "../components/common/LandingPageCommunity";
 import LandingPageRoleSelection from "../components/common/LandingPageRoleSelection";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full"
-    style={{
-            animation: "slideUp 0.6s ease-out",
-          }}
+    <div
+      className="h-screen w-full"
+      style={{
+        animation: "slideUp 0.6s ease-out",
+      }}
     >
-      <main className=" justify-center "
-       >
+      <main className=" justify-center ">
 
+        <LandingPageBanner />
 
-<LandingPageBanner />
-
-<LandingPageRoleSelection />
+        <LandingPageRoleSelection />
         
-
+        <LandingPageCommunity />
+        
       </main>
       <Footer />
     </div>
