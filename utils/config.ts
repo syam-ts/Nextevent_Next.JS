c8bdef1ba26389241ef77df0dfad1a13f72f41df
syam-ts <syamnandhu3@gmail.com>
2025-08-01 20:40:16 +0530

    fix: backend url

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
  
 export const config = {
   frontend_url: process.env.NEXT_PUBLIC_FRONTEND_URL,
-  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
+  backend_url: 'https://nextevent-backend-9n7j.onrender.com/api',
   cloudinay_url: process.env.NEXT_PUBLIC_CLOUDINARY_URL,
   strpe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISABLE_KEY
 };
