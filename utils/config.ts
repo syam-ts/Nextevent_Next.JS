d9f58a9c1fed1f8b9a428d50bfe8708509631262
syam-ts <syamnandhu3@gmail.com>
2025-08-01 20:16:13 +0530

    fix: backend url

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
  
 export const config = {
-  frontend_url: process.env.NEXT_PUBLIC_FRONTEND_URL!,
-  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL!,
+  frontend_url: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  backend_url: process.env.NEXT_PUBLIC_BACKEND_URL,
   cloudinay_url: process.env.NEXT_PUBLIC_CLOUDINARY_URL,
   strpe_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISABLE_KEY
 };
